3899c4f6ebce0f24c4abe77b6dd648c2a9e3f5d5
chirag4206 <76816323+chirag4206@users.noreply.github.com>
2021-11-16 16:52:05 +0530

    Add files via upload

---
+++ class-97/basic.py
@@ -0,0 +1,19 @@
+print("hello")
+name="chirag"
+print(name)
+grade= "9"
+print(grade)
+friend=["ekam","sukhman","bhawan","sahib","jashan"]
+print(friend[3])
+pocketMoney=int(input("enter your pocket money"))
+print(pocketMoney)
+if(pocketMoney>500):
+    print("you can spend evryday")
+elif(pocketMoney>200):
+    print("you can spend every week")
+else:
+    print("you can spend every month")
+count = 5
+while(count>=0):
+    print(count)
+    count = count-1
